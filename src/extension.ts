4d1cb8277b6b7f6204a5ddc00697d88b7c9347db
OnePicecJoker <joker_lizhih@163.com>
2020-08-09 22:14:14 +0800

    tree view demo

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { DepNodeProvider } from './nodeDependencies';
+import { Uri, workspace } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ximalaya-on-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ximalaya-on-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ximalaya on vscode!');
-	});
-
-	context.subscriptions.push(disposable);
+	let workSpaceFloder: any = vscode.workspace.getWorkspaceFolder(vscode.Uri.parse("scheme:with/path"));
+	let rootPath = workSpaceFloder.name;
+	const nodeDependenciesProvider = new DepNodeProvider(rootPath);
+	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 }
 
 // this method is called when your extension is deactivated
